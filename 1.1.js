e0c92260491ce64d2e263af8b73d2b2ca39c9836
Mr-Bie <mrb800424@gmail.com>
2023-08-29 10:50:49 +0330

    FEAT: Declaritive function added

--- 1.1.js
+++ 1.1.js
@@ -1,12 +1,17 @@
-const array = [];
+const array = [0,4,3,2,3,4,5,6];
 
 // Imperative
-const alterArrayI = (array) => {
+const alterArrayI = (_array) => {
     const result = [];
-    for (const element of array) {
-        const newValue = element + Math.floor(Math.random() * array.length);
+    for (const element of _array) {
+        const newValue = element + Math.floor(Math.random() * _array.length);
         result.push(newValue);
     }
     return result;
 }
 
+// Declaritive
+const alterArrayD = (_array) => _array.map(v => v + Math.floor(Math.random() * _array.length));
+
+console.log(alterArrayD(array));
+console.log(alterArrayI(array));
